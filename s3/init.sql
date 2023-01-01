4962e21dbb260ec219d88380128628df2269841e
atdushi <atdushi@example.com>
2023-01-01 15:41:56 +0300

    script fix

--- s3/init.sql
+++ s3/init.sql
@@ -10,3 +10,8 @@ ENGINE=S3('http://127.0.0.1:9010/my-s3bucket/bronze/*', 'Access Key', 'Secret Ke
 
 ALTER TABLE de.`settings` UPDATE `key`='object_storage', value='s3';
 
+CREATE OR REPLACE VIEW de.vw_time_series  AS
+SELECT `timestamp`, `open`, high, low, `close`, volume, symbol
+FROM de.time_series_s3
+GROUP BY `timestamp`, `open`, high, low, `close`, volume, symbol;
+
